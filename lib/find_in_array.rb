aeecc8b5ed57bf43ec494df90bf3a0bb721992db
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-04 20:38:43 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,12 @@
 def find_element_index(array, value_to_find)
   
 end
+
+def output_array_elements(array)
+  counter = 0
+ 
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
+end
